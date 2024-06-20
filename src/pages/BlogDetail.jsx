3d176613edc032d7ddb962e21088d6d5d98b177b
natasja-sluis <natasjag92@gmail.com>
2024-06-20 09:29:46 +0200

    finished form

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,9 +1,8 @@
 import posts from '/src/constants/data.json';
 import {Link, useParams} from "react-router-dom";
+import formatDate from "../helpers/formatDate.js"
 
 function BlogDetail() {
-    console.log(posts);
-
     const {postId} = useParams()
     const post = posts.find(post => post.id === parseInt(postId, 10))
 
@@ -11,7 +10,7 @@ function BlogDetail() {
         <>
             <h1>{post.title}</h1>
             <h2>{post.subtitle}</h2>
-            <p>Geschreven door {post.author} op {post.created}</p>
+            <p>Geschreven door {post.author} op {formatDate(post.created)}</p>
             <p>{post.content}</p>
             <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
             <Link to="/posts" >Terug naar de overzichtspagina</Link>
--- src/pages/NewBlogPost.jsx
+++ src/pages/NewBlogPost.jsx
@@ -1,13 +1,91 @@
-import React from "react";
-import logo from "../assets/logo-white.png";
+import "./newBlogPost.css"
+import { useForm } from "react-hook-form";
+import {useNavigate} from "react-router-dom";
 
 function NewBlogPost() {
+
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+
+    const navigate = useNavigate();
+
+
+ const onSubmit = (data) => {
+     const created = new Date().toISOString();
+     const shares = 0
+     const likes = 0
+     console.log({
+         data,
+         created,
+         shares,
+         likes,
+     });
+
+     console.log("Alle gegevens zijn verzameld!")
+     navigate("/posts");
+ }
+
     return (
         <>
-            <div className="page-container">
-                <img src={logo} alt="Company logo"/>
-                <h1>New!</h1>
-            </div>
+            <h1>Post toevoegen</h1>
+            <form className="form-container" onSubmit={handleSubmit(onSubmit)} >
+                <div className="details-input-container">
+                    <label htmlFor="title-post" className="input-label">
+                        Titel</label>
+                    <input
+                        type="text"
+                        name="title-post"
+                        id="title-post"
+                        {...register("title-post", {required: true})}
+                    />
+
+                </div>
+
+                <div className="details-input-container">
+                    <label className="input-label" htmlFor="sub-title-post">
+                        Subtitel </label>
+                    <input
+                        type="text"
+                        name="sub-title-post"
+                        id="sub-title-post"
+                        {...register("sub-title-post", {required: true})}
+                    />
+
+                </div>
+
+                <div className="details-input-container">
+                    <label className="input-label" htmlFor="author">
+                        Auteur </label>
+                    <input
+                        type="text"
+                        name="author"
+                        id="author"
+                        {...register("author", {required: true})}
+                    />
+
+                </div>
+
+                <div className="details-input-container">
+                    <label className="input-label" htmlFor="message">
+                        Bericht: </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        cols="30"
+                        rows="10"
+                        maxLength="2000"
+                        minLength="300"
+                        {...register("message", {required: true})}
+                    >
+                    </textarea>
+
+                </div>
+
+                <button className="submit-button" type="submit">Versturen</button>
+
+            </form>
         </>
     )
 }
